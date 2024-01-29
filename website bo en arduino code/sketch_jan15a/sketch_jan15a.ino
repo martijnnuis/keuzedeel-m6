8cbb0f3b330db8a62559a232bf121465c9df2f09
martijn <35205@ma-web.nl>
2024-01-29 15:57:13 +0100

    alles

--- website bo en arduino code/sketch_jan15a/sketch_jan15a.ino
+++ website bo en arduino code/sketch_jan15a/sketch_jan15a.ino
@@ -50,11 +50,13 @@ void loop() {
    readString.trim();
 
    if(readString.indexOf("rood") >= 0){
-    Servo1.write(90);
     Servo2.write(0);
-    delay(2000);
-    Servo1.write(0);
+    delay(250);
     Servo2.write(90);
+    delay(2000);
+    Servo1.write(90);
+    delay(155);
+    Servo1.write(0);
    }
  
 
